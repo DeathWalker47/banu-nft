9ed001b6a927abf088815ed50d40e68479b981f0
DeathWalker47 <fizer_00@mail.ru>
2022-03-05 20:36:19 +0300

    Сделал слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
-$(function(){
+// $(function(){
   
+// });
+
+new Swiper(".creators-swiper", {
+  spaceBetween: 30,
+  speed: 800,
+  slidesPerView: 6,
+  navigation: {
+    nextEl: ".creators-swiper-next",
+    prevEl: ".creators-swiper-prev",
+  },
 });
