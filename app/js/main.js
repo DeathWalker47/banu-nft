7a728b455aad968841c3b021c40db5d2ed696b41
DeathWalker47 <fizer_00@mail.ru>
2022-03-14 21:14:38 +0300

    Сделал половину новой страницы. сделал на ней табы

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,12 @@
 // $(function(){
-  
+
 // });
 
 // Swiper slider
 new Swiper(".creators-swiper", {
   spaceBetween: 30,
   speed: 800,
+  loop: true,
   slidesPerView: 6,
   navigation: {
     nextEl: ".creators-next",
@@ -35,31 +36,62 @@ new Swiper(".questions-slider", {
 
 // Filter MisitUp
 
-var mixer = mixitup('.explore__cards', {
-  animation: {
-    easing: 'ease-in-out',
-    duration: 800,
+if (document.querySelector(".explore__cards")) {
+  let mixer = mixitup(".explore__cards", {
+    animation: {
+      easing: "ease-in-out",
+      duration: 800,
+    },
+  });
 }
-});
 
 // Небольшой аккордеон
-const filterTitle = document.querySelectorAll('.filter__item-title');
-filterTitle.forEach(el => {
-  el.addEventListener('click', () => {
+const filterTitle = document.querySelectorAll(".filter__item-title");
+filterTitle.forEach((el) => {
+  el.addEventListener("click", () => {
     const content = el.nextElementSibling;
-    el.classList.toggle('filter__item-title--arrow')
-    content.classList.toggle('content-hidden');
-  })
-})
-
+    el.classList.toggle("filter__item-title--arrow");
+    content.classList.toggle("content-hidden");
+  });
+});
 
 // Выявляем URL страницы
-const linkHeader = document.querySelectorAll('.menu__item-link');
-linkHeader.forEach(el => {
-  if(window.location.pathname == `/${el.getAttribute('href')}`) {
-    document.querySelectorAll('.menu__item-link').forEach(el => {
-      el.classList.remove('menu__item-link--active')
-    })
-    el.classList.add('menu__item-link--active')
+const linkHeader = document.querySelectorAll(".menu__item-link");
+linkHeader.forEach((el) => {
+  if (window.location.pathname == `/${el.getAttribute("href")}`) {
+    document.querySelectorAll(".menu__item-link").forEach((el) => {
+      el.classList.remove("menu__item-link--active");
+    });
+    el.classList.add("menu__item-link--active");
   }
-})
+});
+
+// Простые табы
+const tabsBtn = document.querySelectorAll(".tabs__nav-btn"),
+  tabsItem = document.querySelectorAll(".tabs__content-item"),
+  tabsContent = document.querySelector(".tabs__content");
+
+function onTabClick(el) {
+  el.addEventListener("click", (e) => {
+    let current = e.target;
+    let tabsBtnAttr = current.dataset.tab;
+
+    if (!current.classList.contains("tabs__nav-btn--active")) {
+      tabsBtn.forEach((item) => {
+        item.classList.remove("tabs__nav-btn--active");
+      });
+
+      tabsItem.forEach((item) => {
+        item.classList.remove("tabs__content-item--active");
+      });
+
+      current.classList.add("tabs__nav-btn--active");
+      tabsContent
+        .querySelector(`${tabsBtnAttr}`)
+        .classList.add("tabs__content-item--active");
+    }
+  });
+}
+tabsBtn.forEach(onTabClick);
+
+document.querySelector(".tabs__nav-btn").click();
